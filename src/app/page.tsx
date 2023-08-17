01e696b73be0d5c0df21d8d1c2e4106134198d95
Ramandeep <ramank6238299@gmail.com>
2023-08-17 15:10:02 +0530

    feat - added modal functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,108 @@
+"use client";
+import { useState } from "react";
+
 import "./style/modal.css";
 
 export default function Home() {
+  const timeUnits = ["minutes", "days", "weeks", "months", "years"];
+  const [selectedUnit, setSelectedUnit] = useState("minutes");
+  const [inputValue, setInputValue] = useState(5);
+  const [session, setSession] = useState(false);
+  const [info, setInfo] = useState("");
+
+  const handleUnitChange = (change: number) => {
+    const currentIndex = timeUnits.indexOf(selectedUnit);
+    let newIndex = currentIndex + change;
+
+    if (newIndex < 0) {
+      newIndex = timeUnits.length - 1;
+    } else if (newIndex >= timeUnits.length) {
+      newIndex = 0;
+    }
+
+    setSelectedUnit(timeUnits[newIndex]);
+  };
+
+  const inputHandler = (value: any) => {
+    setInputValue(value);
+  };
+
+  const setSessionHandler = () => {
+    switch (selectedUnit) {
+      case "minutes":
+        {
+          if (inputValue >= 5) {
+            setSession(true);
+            setInfo(
+              `Maximum Lifetime duration for a session is set for ${inputValue} ${selectedUnit}.`
+            );
+          } else {
+            setInfo(`Set duration between 5 minutes and 10 years.`);
+          }
+        }
+        break;
+      case "days":
+        {
+          if (inputValue <= 3650) {
+            setSession(true);
+            setInfo(
+              `Maximum Lifetime duration for a session is set for ${inputValue} ${selectedUnit}.`
+            );
+          } else {
+            setInfo("Set duration between 5 minutes and 10 years");
+          }
+        }
+        break;
+      case "weeks":
+        {
+          if (inputValue <= 521.429) {
+            setSession(true);
+            setInfo(
+              `Maximum Lifetime duration for a session is set for ${inputValue} ${selectedUnit}.`
+            );
+          } else {
+            setInfo("Set duration between 5 minutes and 10 years.");
+          }
+        }
+        break;
+
+      case "months":
+        {
+          if (inputValue <= 120) {
+            setSession(true);
+            setInfo(
+              `Maximum Lifetime duration for a session is set for ${inputValue} ${selectedUnit}.`
+            );
+          } else {
+            setInfo("Set duration between 5 minutes and 10 years.");
+          }
+        }
+        break;
+
+      case "years":
+        {
+          if (inputValue <= 10) {
+            setSession(true);
+            setInfo(
+              `Maximum Lifetime duration for a session is set for ${inputValue} ${selectedUnit}.`
+            );
+          } else {
+            setInfo("Set duration between 5 minutes and 10 years");
+          }
+        }
+        break;
+      default:
+        //@ts-ignore
+        info();
+        "Something went wrong. Set duration between 5 minutes and 10 years.";
+    }
+    if (session) {
+      setSession(false);
+      setInputValue(5);
+      setSelectedUnit("minutes");
+      setInfo("");
+    }
+  };
   return (
     <section className="modal">
       <div className="modal-wrapper">
@@ -39,18 +141,33 @@ export default function Home() {
             </div>
             <div className="toggle-btn">
               <label className="toggle-label">
-                <input type="checkbox" value="" className="sr-only peer" />
+                <input
+                  type="checkbox"
+                  checked={session}
+                  className="sr-only peer"
+                  onChange={() => setSessionHandler()}
+                />
                 <div className="mt-[1px] w-11 h-6 bg-gray-200 rounded-full peer dark:bg-[#7f7f7f33] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
               </label>
             </div>
           </div>
         </div>
         <div className="modal-input-container">
-          <input type="text" className="model-input " />
+          <input
+            type="text"
+            className="model-input "
+            disabled={session}
+            value={inputValue}
+            onChange={(e) => inputHandler(e.target.value)}
+          />
           <div className="input-toggle ">
-            <span>days</span>
+            <span>{selectedUnit}</span>
             <div className="flex-column">
-              <button className="increase">
+              <button
+                className="increase"
+                disabled={session}
+                onClick={() => handleUnitChange(-1)}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -66,7 +183,11 @@ export default function Home() {
                   />
                 </svg>
               </button>
-              <button className="decrease">
+              <button
+                className="decrease"
+                disabled={session}
+                onClick={() => handleUnitChange(1)}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -86,8 +207,12 @@ export default function Home() {
           </div>
         </div>
 
-        <p className="modal-info mb-[24px]">
-          Set duration between 5 minutes and 10 years
+        <p
+          className={
+            info !== "" ? "modal-error mb-[24px]" : "modal-info mb-[24px]"
+          }
+        >
+          {info === "" ? "Set duration between 5 minutes and 10 years." : info}
         </p>
         <div className="divider-hr"></div>
         <div className="space-between">
